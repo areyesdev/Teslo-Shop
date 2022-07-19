f9f885d3eefdf8684cf235d9b490c73d24a2c81e
Andres Reyes <areyesdev@gmail.com>
2022-07-19 14:15:35 -0500

    Add: Home Temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
 import type { NextPage } from 'next'
-import { Typography } from '@mui/material'
+import { CardActionArea, CardMedia, Grid, Typography, Card } from '@mui/material'
 
 import { ShopLayout } from '../components/layouts'
-
+import { initialData } from '../database/products'
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component='img' image={`products/${product.images[0]}`} alt={product.title} />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
 
     </ShopLayout>
   )
