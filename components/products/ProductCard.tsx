e3a9ee1998fd321204697d43d0ed6d06cd82a549
Andres Reyes <areyesdev@gmail.com>
2022-07-19 17:02:30 -0500

    Add: Page product

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,5 @@
-import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from '@mui/material'
+import { Grid, Card, CardActionArea, CardMedia, Box, Typography, Link } from '@mui/material'
+import NextLink from 'next/link'
 import React, { FC, useMemo, useState } from 'react'
 import { IProduct } from '../../interfaces'
 
@@ -21,9 +22,13 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}>
       <Card>
-        <CardActionArea>
-          <CardMedia component='img' image={productImage} alt={product.title} />
-        </CardActionArea>
+        <NextLink href='/product/slug' passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia component='img' className='fadeIn' image={productImage} alt={product.title} />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
       <Box sx={{ mt: 1 }} className='fadeIn'>
---
+++ pages/product/slug.tsx
@@ -0,0 +1,44 @@
+import { Button, Chip, Grid, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+import { ShopLayout } from '../../components/layouts'
+import { initialData } from '../../database/products'
+
+const product = initialData.products[0]
+
+const ProductPage = () => {
+  return (
+    <ShopLayout title={product.title} pageDescription={product.description} >
+      <Grid container spacing={3} >
+        <Grid item xs={12} sm={7}>
+          {/* Slideshow */}
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Box display='flex' flexDirection='column'>
+            {/* Titulos */}
+            <Typography variant='h1' component='h1'>{product.title}</Typography>
+            <Typography variant='subtitle1' component='h2'>{`$${product.price}`}</Typography>
+            {/* cantidad */}
+            <Box sx={{ my: 2 }}>
+              <Typography variant='subtitle2'>Cantidad</Typography>
+              {/* ItemCounter */}
+            </Box>
+            {/* Agregar al carrito */}
+            <Button color='secondary' className='circular-btn'>
+              Agregar al carrito
+            </Button>
+            {/* <Chip label='No hay disponibles' color='error' variant='outlined' /> */}
+            {/* Description */}
+            <Box sm={{ mt: 3 }}>
+              <Typography variant='subtitle2'>Descripción</Typography>
+              <Typography variant='body2'>{product.description}</Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+
+  )
+}
+
+export default ProductPage
