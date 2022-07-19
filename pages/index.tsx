69ce17ac7156e94490a453cf1b14a245bcf99bd5
Andres Reyes <areyesdev@gmail.com>
2022-07-19 16:22:55 -0500

    Add: component ProductCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
-import { CardActionArea, CardMedia, Grid, Typography, Card } from '@mui/material'
+import { Typography } from '@mui/material'
 
 import { ShopLayout } from '../components/layouts'
 import { initialData } from '../database/products'
+import { ProductList } from '../components/products'
 
 const Home: NextPage = () => {
   return (
@@ -10,19 +11,7 @@ const Home: NextPage = () => {
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <Grid container spacing={4}>
-        {
-          initialData.products.map(product => (
-            <Grid item xs={6} sm={4} key={product.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia component='img' image={`products/${product.images[0]}`} alt={product.title} />
-                </CardActionArea>
-              </Card>
-            </Grid>
-          ))
-        }
-      </Grid>
+      <ProductList products={initialData.products as any} />
 
     </ShopLayout>
   )
