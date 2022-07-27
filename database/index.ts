dbefaf7be1a367e8802ae224111df0d2c384bb6b
Andres Reyes <areyesdev@gmail.com>
2022-07-27 16:54:01 -0500

    add getProducts API

---
+++ database/index.ts
@@ -0,0 +1,4 @@
+export * as db from './db'
+
+//Base de datos de prueba 
+export * as seedDatabase from './products'
---
+++ pages/api/products/index.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database'
+import { Product } from '../../../models'
+import { IProduct } from '../../../interfaces/products'
+
+type Data = 
+| { message: string }
+| IProduct[]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return getProducts(req, res)
+  
+    default:
+      return res.status(400).json({
+        message: 'Bad request'
+      })
+  }
+}
+
+const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+  
+  await db.connect();
+  const products =  await Product.find()
+                                  .select('title images price inStock slug -_id')
+
+  await db.disconnect()
+  return res.status(200).json(products)
+}
