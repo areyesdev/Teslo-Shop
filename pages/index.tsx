e0acdedf1d6aa61b77889340efef8acd77552c56
Andres Reyes <areyesdev@gmail.com>
2022-07-28 14:09:26 -0500

    add config global SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,21 @@ import { Typography } from '@mui/material'
 import { ShopLayout } from '../components/layouts'
 import { initialData } from '../database/products'
 import { ProductList } from '../components/products'
+import { useProducts } from '../hooks/useProducts'
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
+  const { products, isLoading } = useProducts('/products')
   return (
-    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
+    <ShopLayout title={'Teslo-Shop - HomePage'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
       <Typography variant='h1' component='h1'>Tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
-
-      <ProductList products={initialData.products as any} />
-
+      {
+        isLoading
+          ? <h1>Cargando...</h1>
+          : <ProductList products={products} />
+      }
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
