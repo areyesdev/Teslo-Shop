90d4dd78e5ddaf362f620147a024da30bae49d70
Andres Reyes <areyesdev@gmail.com>
2022-07-28 14:34:13 -0500

    update component ProductCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -9,6 +9,7 @@ interface Props {
 
 export const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false)
+  const [isImageLoaded, setIsImageLoaded] = useState(false)
 
   const productImage = useMemo(() => {
     return isHovered
@@ -25,13 +26,13 @@ export const ProductCard: FC<Props> = ({ product }) => {
         <NextLink href='/product/slug' passHref prefetch={false}>
           <Link>
             <CardActionArea>
-              <CardMedia component='img' className='fadeIn' image={productImage} alt={product.title} />
+              <CardMedia component='img' className='fadeIn' image={productImage} alt={product.title} onLoad={() => setIsImageLoaded(true)} />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }} className='fadeIn'>
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>{`$${product.price}`}</Typography>
       </Box>
